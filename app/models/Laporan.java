789f8eb94be087cc07dd10a446a8b4e5b9e7c12c
fahmi <vambex.system@gmail.com>
2014-10-22 05:51:51 +0700

    add laporan properties

--- app/models/Laporan.java
+++ app/models/Laporan.java
@@ -1,10 +1,22 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import play.db.ebean.Model;
+// kurang data imagenya
 @Entity
 public class Laporan extends Model {
 	
+	public Long id;
+	public String dataLaporan;
+	public Tanggapan tanggapan;
+	public User user;
+	public List<Komentar> listKomentar;
+	public List<User> listUserPemantau;
+	public String katagoriLaporan;
+	
+	public static Finder<Long, Laporan> finder = new Finder<>(Long.class, Laporan.class);
 
 }
