4a001b9919d54956f7e7e7ea64aeda9cfe8f8387
fahmi <vambex.system@gmail.com>
2014-10-22 05:54:28 +0700

    add anotation to laporan model

--- app/models/Laporan.java
+++ app/models/Laporan.java
@@ -2,19 +2,33 @@ package models;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import play.db.ebean.Model;
 // kurang data imagenya
 @Entity
 public class Laporan extends Model {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long id;
+	@Column
 	public String dataLaporan;
+	@OneToOne(cascade = CascadeType.ALL)
 	public Tanggapan tanggapan;
+	@OneToOne(cascade = CascadeType.ALL)
 	public User user;
+	@OneToMany(cascade = CascadeType.ALL)
 	public List<Komentar> listKomentar;
+	@OneToMany(cascade = CascadeType.ALL)
 	public List<User> listUserPemantau;
+	@Column
 	public String katagoriLaporan;
 	
 	public static Finder<Long, Laporan> finder = new Finder<>(Long.class, Laporan.class);
