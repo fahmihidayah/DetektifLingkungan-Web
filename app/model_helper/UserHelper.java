d08f6d0f0d8a42638b1976fa487c9fe852ba437f
fahmi <vambex.system@gmail.com>
2014-10-28 18:25:48 +0700

    add userhelper class

---
+++ app/model_helper/UserHelper.java
@@ -0,0 +1,26 @@
+package model_helper;
+/*
+ * id: 1
+userName: "f"
+password: "f"
+type: "PEMANTAU"
+name: "fahmi"
+email: "fahmi@rembugan.com"
+status: "i'm a detective"
+jumlahFollowerUser: 0
+jumlahFollowingUser: 0
+}
+ */
+public class UserHelper {
+	public Long id;
+	public String userName;
+	public String password;
+	public String type;
+	public String name;
+	public String email;
+	public String status;
+	public Integer jumlahFollowerUser;
+	public Integer jumlahFollowingUser;
+	public boolean isFollowing;
+
+}
